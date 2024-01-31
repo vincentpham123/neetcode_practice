f07786d4b8a7c7b10a621d5e6fb3fa1f0e81ab80
vincentpham123 <vincentpham32@gmail.com>
2024-01-31 14:52:30 -0800

    max sliding window

--- sliding_window.py
+++ sliding_window.py
@@ -138,5 +138,25 @@ def minWindow(self, s: str, t: str) -> str:
 
         # cant return based on left and right because right will go to the end 
 
+def maxSlidingWindow(self, nums: List[int], k: int) -> List[int]:
+        output = []
+        q = collections.deque()  # index
+        l = r = 0
+        # O(n) O(n)
+        while r < len(nums):
+            # pop smaller values from q
+            while q and nums[q[-1]] < nums[r]:
+                q.pop()
+            q.append(r)
 
+            # remove left val from window
+            if l > q[0]:
+                q.popleft()
+
+            if (r + 1) >= k:
+                output.append(nums[q[0]])
+                l += 1
+            r += 1
+
+        return output
 
