6c8f0afefabe1bb2e27493533e7d067374645444
vincentpham123 <vincentpham32@gmail.com>
2024-02-01 10:55:24 -0800

    min stack class

--- stack.py
+++ stack.py
@@ -16,3 +16,24 @@ class Solution:
                 return False 
             stack.pop()
         return not stack
+class MinStack:
+
+    def __init__(self):
+        self.stack = []
+        self.minStack =[]
+
+    def push(self, val: int) -> None:
+        self.stack.append(val)
+        # now i need to keep track of the current min in the stack 
+        val = min(val, self.minStack[-1] if self.minStack else val)
+        self.minStack.append(val)
+
+    def pop(self) -> None:
+        self.stack.pop()
+        self.minStack.pop()
+
+    def top(self) -> int:
+        return self.stack[-1]
+
+    def getMin(self) -> int:
+        return self.minStack[-1]
