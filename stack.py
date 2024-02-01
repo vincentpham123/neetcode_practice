60874078d003d9cc2d0a6116386918a08615e0a1
vincentpham123 <vincentpham32@gmail.com>
2024-02-01 12:24:45 -0800

    evaluate reverse polish notation

--- stack.py
+++ stack.py
@@ -16,6 +16,25 @@ class Solution:
                 return False 
             stack.pop()
         return not stack
+    def evalRPN(self, tokens: List[str]) -> int:
+
+        stack = []
+        
+        for c in tokens:
+            if c =='+':
+                stack.append(stack.pop() + stack.pop())
+            elif c == '-':
+                a, b = stack.pop(), stack.pop()
+                stack.append(b-a)
+            elif c == '*':
+                stack.append(stack.pop()*stack.pop())
+            elif c == '/':
+                a, b = stack.pop(), stack.pop()
+                stack.append(int(float(b)/a))
+            else:
+                stack.append(int(c))
+        return stack[0]
+                
 class MinStack:
 
     def __init__(self):
