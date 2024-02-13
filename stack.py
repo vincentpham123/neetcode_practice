5157f3f450229eb96752abe47958396e19d25615
vincentpham123 <vincentpham32@gmail.com>
2024-02-13 13:44:06 -0800

    generate parenthesis

--- stack.py
+++ stack.py
@@ -82,7 +82,26 @@ class Solution:
             if len(stack) >= 2 and stack[-1] <= stack[-2]:
                 stack.pop()
         return len(stack)
-                
+    def generateParenthesis(self, n: int) -> List[str]:
+            stack = []
+            res = []
+
+            def backtrack(openN, closedN):
+                if openN == closedN == n:
+                    res.append("".join(stack))
+                    return
+
+                if openN < n:
+                    stack.append("(")
+                    backtrack(openN + 1, closedN)
+                    stack.pop()
+                if closedN < openN:
+                    stack.append(")")
+                    backtrack(openN, closedN + 1)
+                    stack.pop()
+
+            backtrack(0, 0)
+            return res             
 class MinStack:
 
     def __init__(self):
