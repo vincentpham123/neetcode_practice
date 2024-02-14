b01cbe138e2d4fe90d6734515ad2ff4da2d83402
vincentpham123 <vincentpham32@gmail.com>
2024-02-14 15:18:36 -0800

    2d binary search

--- binary_search.py
+++ binary_search.py
@@ -15,3 +15,33 @@ class Solution:
                 else:
                     return mid 
             return -1
+    def searchMatrix(self, matrix: List[List[int]], target: int) -> bool:
+            colCount = len(matrix[0])
+            rowCount = len(matrix)
+            left = 0 
+            right = colCount * rowCount - 1 
+            # this will provide the right of the 2d matrix
+
+            # now i just need a way to find the midpoint and compare 
+            while left <= right:
+                midPoint = left + ((right-left)//2)
+
+                #this is the midpoint of an array
+                #example [[1,3,5,7],[10,11,16,20],[23,30,34,60]]
+                # 0 + (12-0/2) = 6
+                # with the midpoint, I now need to dive into the 2d matrix
+                # what is the connection??
+                # the midpoint is matrix[1][2]
+                midRow = midPoint // colCount 
+                # 6 / 4 which is 1
+                midCol = midPoint % colCount
+                # 6 % 4 which is 1 
+
+                if matrix[midRow][midCol] < target:
+                    # i know that it is on the right side
+                    left = midPoint+1 
+                elif matrix[midRow][midCol] > target:
+                    right = midPoint - 1 
+                else:
+                    return True 
+            return False
