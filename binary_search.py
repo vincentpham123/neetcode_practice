fa6e0ce9e4ca51cef6bb74b6b038d4be1110b269
vincentpham123 <vincentpham32@gmail.com>
2024-02-21 21:46:32 -0800

    timemap

--- binary_search.py
+++ binary_search.py
@@ -137,4 +137,32 @@ class Solution:
             
             return -1 
 
-            
+        
+class TimeMap:
+
+    def __init__(self):
+        self.keystore = {}
+
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        if key not in self.keystore:
+            self.keystore[key] = []
+        self.keystore[key].append([value, timestamp])
+
+    def get(self, key: str, timestamp: int) -> str:
+        res, values = '', self.keystore.get(key,[])
+        # if the key store, doesnt have the values, just return an empty list 
+
+        l, r = 0, len(values) - 1 
+        # the trick to this is that all numbers will be sorted by smallest to greatest based on the timestamp
+        while l <= r:
+            m = (l+r)//2
+            if values[m][1] == timestamp:
+                return values[m][0]
+            if values[m][1] <= timestamp:
+                res=values[m][0]
+                l = m + 1 
+            else:
+                r = m - 1
+        return res
+        
