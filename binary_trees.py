aa69eaaaab13a6a542ee5fd305ecd8232498e02f
vincentpham123 <vincentpham32@gmail.com>
2024-03-18 16:36:13 -0700

    serialize derialize

--- binary_trees.py
+++ binary_trees.py
@@ -173,3 +173,52 @@ class Solution:
         # i know the next in preorder is left
         # i also know that 9 is the only node on the left 
         return root
+# Definition for a binary tree node.
+# class TreeNode(object):
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Codec:
+
+    def serialize(self, root):
+        """Encodes a tree to a single string.
+        
+        :type root: TreeNode
+        :rtype: str
+        """
+        result = []
+
+        def dfs(root):
+            if not root:
+                result.append('n')
+                return 
+            
+            result.append(str(root.val))
+
+            dfs(root.left)
+            dfs(root.right)
+        dfs(root)
+        print(','.join(result))
+        return ','.join(result)
+
+    def deserialize(self, data):
+        """Decodes your encoded data to tree.
+        
+        :type data: str
+        :rtype: TreeNode
+        """
+        vals = data.split(',')
+        self.i = 0
+
+        def dfs():
+            if vals[self.i] == 'n':
+                self.i += 1 
+                return None 
+            node = TreeNode(int(vals[self.i]))
+            self.i += 1
+            node.left = dfs()
+            node.right = dfs()
+            return node
+        return dfs()
