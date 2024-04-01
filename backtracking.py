e644644532808c43d59b36dd9d4cbf4ab51b384e
vincentpham123 <vincentpham32@gmail.com>
2024-04-01 15:13:10 -0700

    permutations dfs

--- backtracking.py
+++ backtracking.py
@@ -60,3 +60,30 @@ class Solution:
 
         backtrack(0)
         return res
+def permute(self, nums: List[int]) -> List[List[int]]:
+
+    res= []
+
+    
+    # the idea is that for each number, we remove until we have a single, then add 
+    if len(nums)==1:
+            # we have reached the base case from all the recursive calls 
+        return [nums[:]]
+    for i in range(len(nums)):
+        # this should let us 
+        
+            # in python this returns a copt 
+        n = nums.pop(0)
+
+        perms = self.permute(nums)
+        # remember that nums is now [2,3]
+
+        for i in perms:
+            i.append(n)
+            # now we add back the number we have popped
+        res.extend(perms)
+        nums.append(n)
+    return res
+        
+        # [1,2,3]
+        # for the first loop, we pop at 1 
