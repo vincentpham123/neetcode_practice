5b42077d516a9571e42c9e32f5f00593c68fab4a
vincentpham123 <vincentpham32@gmail.com>
2024-04-02 12:11:12 -0700

    subsets ii

--- backtracking.py
+++ backtracking.py
@@ -87,3 +87,23 @@ def permute(self, nums: List[int]) -> List[List[int]]:
         
         # [1,2,3]
         # for the first loop, we pop at 1 
+class Solution:
+    def subsetsWithDup(self, nums: List[int]) -> List[List[int]]:
+        res = []
+        nums.sort()
+
+        def backtrack(i,subset):
+
+            if i == len(nums):
+                res.append(subset[::])
+                return 
+            
+            subset.append(nums[i])
+            backtrack(i+1, subset)
+            subset.pop()
+
+            while i+1 < len(nums) and nums[i] == nums[i+1]:
+                i+=1
+            backtrack(i+1, subset)
+        backtrack(0,[])
+        return res
