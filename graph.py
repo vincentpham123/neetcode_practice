a2f9b2a9beda180b3707b63118dbe0427f3d74bb
vincentpham123 <vincentpham32@gmail.com>
2024-04-05 11:16:31 -0700

    oranges

--- graph.py
+++ graph.py
@@ -196,3 +196,48 @@ class Solution:
             for c in range(cols):
                 if board[r][c]=='T':
                     board[r][c] = 'O'
+class Solution:
+    def orangesRotting(self, grid: List[List[int]]) -> int:
+        q = collections.deque()
+
+        time = 0
+        fresh = 0
+
+        rows, cols = len(grid), len(grid[0])
+
+        for r in range(rows):
+            for c in range(cols):
+                # check for fresh and rottens
+                # rottens we append to queue, 
+                # fresh we add tot he fresh count 
+
+                if grid[r][c] == 1:
+                    fresh += 1 
+                if grid[r][c] == 2:
+                    q.append((r,c))
+        
+        directions = [[1,0],[-1,0], [0,1], [0,-1]]
+        while fresh > 0 and q:
+            
+            # i need to pop everything in the queue
+            length = len(q)
+
+            for i in range(length):
+                r, c = q.popleft()
+
+                for dr, dc in directions:
+                    row, col = r + dr, c + dc
+
+                    if (
+                        row >=0
+                        and col >= 0
+                        and row < rows
+                        and col < cols
+                        and grid[row][col] == 1
+                    ):
+                        grid[row][col] = 2
+                        q.append((row,col))
+                        fresh -= 1 
+            time += 1
+        
+        return time if fresh==0 else -1
