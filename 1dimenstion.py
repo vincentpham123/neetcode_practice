cda94b9e2ffcd9fb5bd8eb0762ad3bbcbe52d482
vincentpham123 <vincentpham32@gmail.com>
2024-04-17 10:54:29 -0700

    min cost climbing

--- 1dimenstion.py
+++ 1dimenstion.py
@@ -9,3 +9,14 @@ class Solution:
             two = temp 
             # adding up all steps as we reach the end 
         return one
+
+class Solution:
+    def minCostClimbingStairs(self, cost: List[int]) -> int:
+        
+
+        for i in range(len(cost)-3, -1, -1):
+            # need to go backwards and start at the 3rd to last 
+
+            cost[i] += min(cost[i+1], cost[i+2])
+        
+        return min(cost[0], cost[1])
