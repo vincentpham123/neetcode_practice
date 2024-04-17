e48528a88c138bb6095fb6bfb9eeae33d4842887
vincentpham123 <vincentpham32@gmail.com>
2024-04-17 11:21:25 -0700

    house robber

--- 1dimenstion.py
+++ 1dimenstion.py
@@ -20,3 +20,18 @@ class Solution:
             cost[i] += min(cost[i+1], cost[i+2])
         
         return min(cost[0], cost[1])
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        
+        house1, house2 = 0 ,0 
+        # [house1, house2, n, n+1, n+2]
+        # the idea is that each number has two decisions
+        # we can either add the the house down 2, or just rob the current house 
+        
+        for n in nums:
+            temp = max(n + house1, house2)
+
+            house1 = house2 
+            house2 = temp
+        
+        return house2
