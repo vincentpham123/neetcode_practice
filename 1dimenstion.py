eb6530101154915e0f60a5e6943f983c2ca8e8c8
vincentpham123 <vincentpham32@gmail.com>
2024-04-22 10:38:49 -0700

    house robber

--- 1dimenstion.py
+++ 1dimenstion.py
@@ -86,3 +86,24 @@ class Solution:
             # as we go down the steps, we will dynamically update 
         
         return min(cost[0], cost[1])
+class Solution:
+    def rob(self, nums: List[int]) -> int:
+        house1, house2 = 0 ,0 
+
+        for i in nums:
+
+            temp = max(house1+i, house2)
+            # house1 and house2 are imaginery houses
+            # this allows me to imitate the initial condition
+            # house1 represents the orevious house over, and house2 is the previous house
+            # house1 + i means that we are robbing the current house along with the previous house over
+            # house2 represents stealing the previous house 
+
+            # now we have to update house1 and house2 
+
+            # house1, house2, i, i+1
+            
+            # we need house1 to be the next house pver
+            house1 = house2 
+            house2 = temp 
+        return house2
