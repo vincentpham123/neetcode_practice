7d4ccfaa5987a63313b484260c15d0252d0e1125
vincentpham123 <vincentpham32@gmail.com>
2024-05-01 14:27:26 -0700

    reviewing stacks

--- stack.py
+++ stack.py
@@ -123,3 +123,18 @@ class MinStack:
 
     def getMin(self) -> int:
         return self.minStack[-1]
+class Solution:
+    def isValid(self, s: str) -> bool:
+        
+        stack = []
+
+        Map = {")": "(", "]": "[", "}": "{"}
+
+        for c in s:
+            if c not in Map:
+                stack.append(c)
+                continue
+            if not stack or stack[-1] != Map[c]:
+                return False 
+            stack.pop()
+        return not stack
