f4c08f745b3a2552064e1751505c503299fa01a1
vincentpham123 <vincentpham32@gmail.com>
2024-04-30 18:26:22 -0700

    subarray

--- 1dimenstion.py
+++ 1dimenstion.py
@@ -277,3 +277,21 @@ class Solution:
             ):
                 dp[i] += dp[i + 2]
         return dp[0]
+    
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        res = nums[0]
+
+        curMin, curMax = 1,1 
+
+        for n in nums:
+
+            tmp = curMax * n 
+
+            curMax = max(n * curMax, n * curMin, n)
+
+            curMin = min(tmp, n * curMin, n)
+
+            res = max(res, curMax)
+        
+        return res
