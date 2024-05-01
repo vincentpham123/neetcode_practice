7b017e5b7f3fe3fecb664dc3af050f2f413fb126
vincentpham123 <vincentpham32@gmail.com>
2024-04-30 21:08:24 -0700

    maxPrduct

--- dynamic_programming.py
+++ dynamic_programming.py
@@ -25,3 +25,24 @@ def min_change_helper(amount, coins, memo):
     
   memo[amount] = min_amount 
   return memo[amount]
+
+class Solution:
+    def coinChange(self, coins: List[int], amount: int) -> int:
+        
+        dp = [amount + 1] * (amount + 1)
+
+        dp[0] = 0
+
+        # that is the base case
+
+        for a in range(1, amount + 1):
+            # gonna iterate and find the min amount for all these coin amounts basically 
+
+            for i in coins:
+                # so i now iterate through the coins and check if they can equal a 
+
+                if a - i >= 0:
+                    dp[a] = min(dp[a], 1+ dp[a-i])
+                    # the idea is that a - i coins should already been found and is already the min combination from the coins available 
+        
+        return dp[amount] if dp[amount] != amount +1 else -1
