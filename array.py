c9ed88e2d4aa744416b3dae266ec5dd1d66098bd
vincentpham123 <vincentpham32@gmail.com>
2024-05-07 10:22:06 -0700

    replacElements

--- array.py
+++ array.py
@@ -34,3 +34,14 @@ class Solution:
             for n in nums:
                 res.append(n)
         return res
+class Solution:
+    def replaceElements(self, arr: List[int]) -> List[int]:
+        
+
+        rightMax = -1
+
+        for i in range(len(arr)-1,-1,-1):
+            newMax = max(rightMax, arr[i])
+            arr[i] = rightMax
+            rightMax = newMax
+        return arr
