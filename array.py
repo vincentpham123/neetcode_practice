453ec9818b6ca97227bc264f0ae7c4ed416c0ead
vincentpham123 <vincentpham32@gmail.com>
2024-05-07 10:55:24 -0700

    more array problems

--- array.py
+++ array.py
@@ -44,4 +44,35 @@ class Solution:
             newMax = max(rightMax, arr[i])
             arr[i] = rightMax
             rightMax = newMax
-        return arr
+        return arr 
+class Solution:
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        
+        # the best thing to do is to have another array
+        # that is keeping track of the product to the right
+
+        # so we can keep an array that stores the product to the left of i in i 
+
+        res = [1] * len(nums)
+
+        for i in range(1,len(res)):
+            res[i] = res[i-1]* nums[i-1]
+
+            # looking at this, we are basically storing the product to the left 
+            # at i, so at res[1] = res[0] (1) * nums[1]
+            # we continue this 
+            # we dont update the first index, because there is nothing to the left 
+        postFix = 1
+
+        # now we have to fix it
+        # the idea is to go from the right
+        # now that we have every product from the left
+        # we muliply it with the product from the right
+
+        for i in range(len(res)-1,-1,-1):
+            res[i] *= postFix
+            postFix *= nums[i]
+            # we have to update postFix to reflect the product from the right
+
+            # next loop, itll be 4, then 12, then 24
+        return res
