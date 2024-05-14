9f9726fb6ec50da6b4ebe89251779dd1661ad679
vincentpham123 <vincentpham32@gmail.com>
2024-05-14 15:02:19 -0700

    valid sudoku

--- array.py
+++ array.py
@@ -137,3 +137,29 @@ class Solution:
                 streak +=1
             result = max(streak, result)
         return result
+
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        rows = collections.defaultdict(set)
+        cols = collections.defaultdict(set)
+        square = collections.defaultdict(set)
+
+        # i bascially just need to check if there are repitions in the rows, cols and square
+
+        for r in range(9):
+            for c in range(9):
+
+                if board[r][c]=='.':
+                    continue
+                elif (
+                    board[r][c] in rows[r] or 
+                    board[r][c] in cols[c] or
+                    board[r][c] in square[(r//3, c//3)]
+                ):
+                    return False
+                else:
+                    rows[r].add(board[r][c])
+                    cols[c].add(board[r][c])
+                    square[(r//3, c//3)].add(board[r][c])
+        return True
