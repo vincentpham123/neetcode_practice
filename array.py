1fd9fcb7e95784621fa1246fbb36595180ae4c9b
vincentpham123 <vincentpham32@gmail.com>
2024-05-14 10:06:23 -0700

    brick wall

--- array.py
+++ array.py
@@ -76,3 +76,14 @@ class Solution:
 
             # next loop, itll be 4, then 12, then 24
         return res
+
+class Solution:
+    def leastBricks(self, wall: List[List[int]]) -> int:
+        countGap = {0:0}
+
+        for r in wall:
+            total = 0 
+            for b in r[:-1]:
+                total += b
+                countGap[total] = 1 + countGap.get(total, 0)
+        return len(wall) - max(countGap.values())
