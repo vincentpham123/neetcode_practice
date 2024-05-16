78a9c35885ea1ebdfc9ee7b161532a43eef4f68a
vincentpham123 <vincentpham32@gmail.com>
2024-05-16 14:51:04 -0700

    word dictionary

--- tries.py
+++ tries.py
@@ -85,9 +85,49 @@ class Trie:
             cur = cur.children[index]
         return True
 
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.end = False
+class WordDictionary:
 
-# Your Trie object will be instantiated and called as such:
-# obj = Trie()
-# obj.insert(word)
+    def __init__(self):
+        self.root = TrieNode()
+
+    def addWord(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            if c not in cur.children:
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
+        cur.end = True
+
+    def search(self, word: str) -> bool:
+        #dfs logic
+        #we need to get to the end
+        # we know if we run into a '.'
+
+        def dfs(j, root):
+            cur = root
+
+            for i in range(j, len(word)):
+                c= word[i]
+                if c == '.':
+                    for child in cur.children.values():
+                        if dfs(i+1, child):
+                            return True
+                    return False
+                else:
+                    if c not in cur.children:
+                        return False
+                    cur = cur.children[c]
+            return cur.end
+        return dfs(0, self.root)
+
+
+
+# Your WordDictionary object will be instantiated and called as such:
+# obj = WordDictionary()
+# obj.addWord(word)
 # param_2 = obj.search(word)
-# param_3 = obj.startsWith(prefix)
