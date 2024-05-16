0ba7a2744980998651c5843ddcfd7fa453132f9e
vincentpham123 <vincentpham32@gmail.com>
2024-05-16 14:20:13 -0700

    with index

--- tries.py
+++ tries.py
@@ -42,6 +42,50 @@ class Trie:
         return True
 
 
+class TrieNode():
+    def __init__(self):
+        self.children = [None]*26
+        self.end = False
+class Trie:
+
+    def __init__(self):
+        self.root = TrieNode()
+
+    def insert(self, word: str) -> None:
+        cur = self.root
+
+        for c in word:
+            index = ord(c) - ord('a')
+            # this will return between [0:25]
+            if cur.children[index] == None:
+                cur.children[index] = TrieNode()
+            cur = cur.children[index]
+        
+        cur.end = True
+
+    def search(self, word: str) -> bool:
+        cur = self.root 
+
+        for c in word:
+            index = ord(c) - ord('a')
+            if cur.children[index] == None:
+                return False
+            cur = cur.children[index]
+        return cur.end
+
+    def startsWith(self, prefix: str) -> bool:
+        
+        cur = self.root
+
+        for c in prefix:
+            index = ord(c) - ord('a')
+
+            if cur.children[index]== None:
+                return False
+            cur = cur.children[index]
+        return True
+
+
 # Your Trie object will be instantiated and called as such:
 # obj = Trie()
 # obj.insert(word)
