0a05c76581b12ab587012bce33c8ea5765adf990
vincentpham123 <vincentpham32@gmail.com>
2024-05-15 19:28:02 -0700

    3sum review

--- array.py
+++ array.py
@@ -182,3 +182,40 @@ class Solution:
 
             left += b
         return result
+    
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        
+
+        # the trick to threeSum like all sums is to sort
+
+        nums.sort()
+
+        # the way to avoid diplicate sets is to move the pointer so its not the same as the last or greater than 0
+        res = []
+        
+        for i, a in enumerate(nums):
+            if a > 0:
+                break
+            
+            if i > 0. and a == nums[i-1]:
+                continue 
+            
+            l,r = i+1, len(nums)-1
+
+            while l < r:
+                threesum = a + nums[l] + nums[r]
+                if threesum>0:
+                    r-=1
+                elif threesum<0:
+                    l+=1
+                else:
+                    res.append([a, nums[l], nums[r]])
+                    l+=1
+                    r-=1
+                    
+                    # need to skeep duplicates
+                    while nums[l] == nums[l-1] and l <r:
+                        l+=1
+        return res
+            
