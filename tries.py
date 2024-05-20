b91861e789ad6ad06ad9464ba8539082e44776e3
vincentpham123 <vincentpham32@gmail.com>
2024-05-20 09:52:31 -0700

    word search 2 with refs

--- tries.py
+++ tries.py
@@ -149,6 +149,8 @@ class WordDictionary:
     def search(self, word: str) -> bool:
         # try with BFS
 
+        def search(self, word: str) -> bool:
+        
         q = collections.deque([(self.root,0)])
 
         while q:
@@ -157,7 +159,9 @@ class WordDictionary:
             # i now need to check the word[i]
 
             if i == len(word):
-                return True
+                if node.end:
+                    return True
+                continue
             if word[i]=='.':
                 for child in node.children.values():
                     q.append((child,i+1))
@@ -166,9 +170,83 @@ class WordDictionary:
                     q.append((node.children[word[i]],i+1))
         return False
 
-
-
 # Your WordDictionary object will be instantiated and called as such:
 # obj = WordDictionary()
 # obj.addWord(word)
 # param_2 = obj.search(word)
+class TrieNode:
+    def __init__(self):
+        self.children = {}
+        self.isWord = False
+        self.ref = 0
+
+        #self.ref will be used to mark that the word has been found
+
+    def addWord(self, word):
+        curr = self
+        curr.ref += 1
+
+        for c in word:
+            if c not in curr.children:
+                curr.children[c] = TrieNode()
+            curr = curr.children[c]
+            curr.ref += 1
+        curr.isWord=True 
+    def removeWord(self, word):
+        cur = self
+        cur.ref -= 1
+        for c in word:
+            if c in cur.children:
+                cur = cur.children[c]
+                cur.ref -= 1
+        # used to keep track of the words associated with each node
+    
+        
+
+
+class Solution:
+    def findWords(self, board: List[List[str]], words: List[str]) -> List[str]:
+        
+        root = TrieNode()
+
+        for word in words:
+            root.addWord(word)
+        
+        ROWS, COLS = len(board), len(board[0])
+
+        res, visited = set(), set()
+
+        def dfs(r, c, node, word):
+
+            if (
+                r < 0 or
+                c < 0 or
+                r == ROWS or
+                c == COLS or
+                board[r][c] not in node.children or
+                node.children[board[r][c]].ref < 1 or
+                (r, c) in visited 
+
+            ):
+                return 
+            visited.add((r, c))
+
+            node = node.children[board[r][c]]
+            word += board[r][c]
+            
+            if node.isWord:
+                node.Word = False
+                res.add(word)
+                root.removeWord(word)
+
+            dfs(r + 1, c, node, word)
+            dfs(r - 1, c, node, word)
+            dfs(r, c + 1, node, word)
+            dfs(r, c - 1, node, word)
+            visited.remove((r, c))
+            #backtracking
+        
+        for r in range(ROWS):
+            for c in range(COLS):
+                dfs(r, c, root, '')
+        return list(res)
